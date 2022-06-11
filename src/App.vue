3c2bf4fcbd694df1cbae85f37ccd9ff105793c7b
Thomas Bnt <thomasbnt@protonmail.com>
2022-06-12 01:29:13 +0200

    Added icon WM animated and .title

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <section class='header__section'>
-      <h1>Awesome Web Monetization</h1>
+      <div class='title'>
+        <img src='/src/assets/img/wm_small_icon_animated.svg' alt='Web Monetization logo' width='100' height='100'>
+        <h1>Awesome Web Monetization</h1>
+      </div>
       <h2>Stuffs about Web Monetization. Packages, articles, documentation links and others tools.</h2>
       <a href='https://github.com/thomasbnt/awesome-web-monetization' class='btn-main'>
         <i class='fa fa-github' aria-hidden='true'></i> See the list on GitHub
@@ -28,4 +31,12 @@ export default {
 </script>
 <style lang='scss'>
 @import './assets/scss/main.scss';
+.title {
+  display: flex;
+  align-items: center;
+  margin-bottom: 2rem;
+  > img {
+    margin-right: 1rem;
+  }
+}
 </style>
